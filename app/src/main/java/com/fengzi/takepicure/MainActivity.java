9e65161220d4a2d1d05771e28264b7e732098baf
VinRichard <yanff2000@163.com>
2017-10-31 16:49:34 +0800

    提交试用说明文档
    1111111111111111111111

--- app/src/main/java/com/fengzi/takepicure/MainActivity.java
+++ app/src/main/java/com/fengzi/takepicure/MainActivity.java
@@ -1,18 +1,7 @@
 package com.fengzi.takepicure;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -77,13 +66,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void initPhoto() {
-//        if (ContextCompat.checkSelfPermission(MainActivity.this,
-//                Manifest.permission.READ_EXTERNAL_STORAGE)
-//                != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
-//        } else {
-            openPhoto();
-//        }
+        openPhoto();
     }
 
     void openPhoto() {
@@ -126,41 +109,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         ImageSelector.open(MainActivity.this, builder.build());   // 开启图片选择器
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 100:
-
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    openPhoto();
-                } else {
-                    boolean b = shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE);
-                    if (!b) {
-                        new AlertDialog.Builder(MainActivity.this)
-                                .setTitle("存储权限不可用")
-                                .setMessage("请在-应用设置-权限-中，允许支付宝使用存储权限来保存用户数据")
-                                .setPositiveButton("立即开启", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                        Intent intent = new Intent();
-                                        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                        Uri uri = Uri.fromParts("package", getPackageName(), null);
-                                        intent.setData(uri);
-                                        startActivityForResult(intent, 123);
-                                    }
-                                }).setNegativeButton("取消", null).setCancelable(false).show();
-                    } else {
-                        toast("权限拒绝将影响应用正常使用");
-                    }
-                }
-                return;
-        }
-
     }
 
     @Override
